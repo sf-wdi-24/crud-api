b960172fc1c69ed4e285e79d0a6b3a7c5d79f641
cameronjacoby <cameron.jacoby@gmail.com>
2015-11-02 09:31:50 -0800

    pull out catchphrasely code

--- 4_solution_nested_resources/models/note.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var NoteSchema = new Schema({
-  text: String
-});
-
-var Note = mongoose.model('Note', NoteSchema);
-module.exports = Note;
---
+++ server.js
@@ -0,0 +1,84 @@
+// require express and other modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+// configure bodyParser (for receiving form data)
+app.use(bodyParser.urlencoded({extended: true}));
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/crud-api');
+
+// require `Phrase` model
+var Phrase = require('./models/phrase');
+
+
+// API ROUTES
+
+// get all phrases
+app.get('/api/phrases', function (req, res) {
+  // find all phrases in db
+  Phrase.find(function (err, phrases) {
+    res.json(phrases);
+  });
+});
+
+// create new phrase
+app.post('/api/phrases', function (req, res) {
+  // create new phrase with form data (`req.body`)
+  var newPhrase = new Phrase({
+    word: req.body.word,
+    definition: req.body.definition
+  });
+
+  // save new phrase in db
+  newPhrase.save(function (err, savedPhrase) {
+    res.json(savedPhrase);
+  });
+});
+
+// get one phrase
+app.get('/api/phrases/:id', function (req, res) {
+  // set the value of the id
+  var targetId = req.params.id;
+
+  // find phrase in db by id
+  Phrase.findOne({_id: targetId}, function (err, foundPhrase) {
+    res.json(foundPhrase);
+  });
+});
+
+// update phrase
+app.put('/api/phrases/:id', function (req, res) {
+  // set the value of the id
+  var targetId = req.params.id;
+
+  // find phrase in db by id
+  Phrase.findOne({_id: targetId}, function (err, foundPhrase) {
+    // update the phrase's word and definition
+    foundPhrase.word = req.body.word;
+    foundPhrase.definition = req.body.definition;
+
+    // save updated phrase in db
+    foundPhrase.save(function (err, savedPhrase) {
+      res.json(savedPhrase);
+    });
+  });
+});
+
+// delete phrase
+app.delete('/api/phrases/:id', function (req, res) {
+  // set the value of the id
+  var targetId = req.params.id;
+
+  // find phrase in db by id and remove
+  Phrase.findOneAndRemove({_id: targetId}, function (err, deletedPhrase) {
+    res.json(deletedPhrase);
+  });
+});
+
+// set server to localhost:3000
+app.listen(3000, function () {
+  console.log('server started on localhost:3000');
+});
