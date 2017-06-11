ce3252ff6076b77ce297a1ee0ac165f27f02136b
bgveenstra <bgveenstra@gmail.com>
2017-06-10 20:37:26 -0700

    additional error case tests for book show

--- test/book.js
+++ test/book.js
@@ -94,28 +94,27 @@ describe('Books Controller', function(){
 				})
 			});
 			it('responds with an object that matches a saved book', function(done){
-					request(app)
-					.post('/books')
-	    			.send({ 
-	    				title: 'yeah',
-	    				author: 'okay',
-	    				image: 'kittens.png',
-	    				releaseDate: '2015'
-	    			})
-					.expect(function(res){
-						expect(res.body).to.have.all.keys('title', 'author', 'image', 'releaseDate', '_id', '__v');
-						expect(res.body.title).to.be.a('string');
-						expect(res.body.title).to.equal('yeah');
-						expect(res.body.author).to.be.a('string');
-						expect(res.body.author).to.equal('okay');
-						expect(res.body.image).to.be.a('string');
-						expect(res.body.image).to.equal('kittens.png');
-						expect(res.body.releaseDate).to.be.a('string');
-						expect(res.body.releaseDate).to.equal('2015');
-						expect(res.body.__v).to.equal(0);
-					})
-					.end(done);
-
+				request(app)
+				.post('/books')
+    			.send({ 
+    				title: 'yeah',
+    				author: 'okay',
+    				image: 'kittens.png',
+    				releaseDate: '2015'
+    			})
+				.expect(function(res){
+					expect(res.body).to.have.all.keys('title', 'author', 'image', 'releaseDate', '_id', '__v');
+					expect(res.body.title).to.be.a('string');
+					expect(res.body.title).to.equal('yeah');
+					expect(res.body.author).to.be.a('string');
+					expect(res.body.author).to.equal('okay');
+					expect(res.body.image).to.be.a('string');
+					expect(res.body.image).to.equal('kittens.png');
+					expect(res.body.releaseDate).to.be.a('string');
+					expect(res.body.releaseDate).to.equal('2015');
+					expect(res.body.__v).to.equal(0);
+				})
+				.end(done);
 			});
 			it('updates the Watcher tally for book CREATE requests', function(done){
 				db.Watcher.count({crudAction: 'CREATE', modelType: 'Book'}, function(err,startCount){
@@ -134,25 +133,27 @@ describe('Books Controller', function(){
 			});
 		})
 		describe(context('error'), function(){
-			it('responds with status code 500', function(done){
-				request(app)
-				.post('/books')
-   				.send({})  // assumes some validation for a field not missing
-				.expect(function(res) {
-					expect(res.status).to.equal(500);
-				})
-				.end(done);
-			});
-			it('responds with an object that has a string value for the key "error"', function(done){
-				request(app)
-				.post('/books')
-   				.send({})  // assumes some validation for a field not missing
-				.expect(function(res){
-					expect(res.body).to.have.own.property('error');
-					expect(res.body.error).to.be.a('string');
-				})
-				.end(done);
-			});
+			it('responds with status code 500');
+			it('responds with an object that has a string value for the key "error"');
+			// it('responds with status code 500', function(done){
+			// 	request(app)
+			// 	.post('/books')
+			//	.send({})  // assumes some validation for a field not missing
+			// 	.expect(function(res) {
+			// 		expect(res.status).to.equal(500);
+			// 	})
+			// 	.end(done);
+			// });
+			// it('responds with an object that has a string value for the key "error"', function(done){
+			// 	request(app)
+			// 	.post('/books')
+   			//  .send({})  // assumes some validation for a field not missing
+			// 	.expect(function(res){
+			// 		expect(res.body).to.have.own.property('error');
+			// 		expect(res.body.error).to.be.a('string');
+			// 	})
+			// 	.end(done);
+			// });
 			it('responds with an object that has key "error" with the error message as the value');
 		})
 	})
@@ -176,12 +177,46 @@ describe('Books Controller', function(){
 		it('looks up a book based on parameter from the request path');
 		// @TODO - actually primarily testing controllers/utils.js with these first 3 contexts
 		describe(context('book find error'), function(){
-			it('responds with status code 500');
-			it('responds with an object that has key "error" with the error message as the value');
+			it('responds with status code 500', function(done){
+				request(app)
+				.get('/books/not-an-object-id')
+				.expect(function(res){
+					expect(res.status).to.equal(500);
+				})
+				.end(done);
+			});
+			it('responds with an object that has key "error" with the error message as the value', function(done){
+				request(app)
+				.get('/books/not-an-object-id')
+				.expect(function(res){
+					expect(res.type).to.equal('application/json');
+					expect(res.body).to.have.own.property('error');
+					expect(res.body.error).to.be.a('string');
+				})
+				.end(done);
+			});
 		})
 		describe(context('found book is null'), function(){
-			it('responds with status code 404');
-			it('responds with an object that has key "error" with the error message "Nothing found by this ID." as the value');
+			it('responds with status code 404', function(done){
+				var fakeId = '593cb53d0b376629bb4d7738';
+				request(app)
+				.get('/books/' + fakeId)
+				.expect(function(res){
+					expect(res.status).to.equal(404);
+				})
+				.end(done);
+			});
+			it('responds with a JSON object that has key "error" with the error message "Nothing found by this ID." as the value', function(done){
+				var fakeId = '593cb53d0b376629bb4d7738';
+				request(app)
+				.get('/books/' + fakeId)
+				.expect(function(res){
+					expect(res.type).to.equal('application/json');
+					expect(res.body).to.have.own.property('error');
+					expect(res.body.error).to.be.a('string');
+				})
+				.end(done);
+			});
 		})
 		describe(context('success'), function(){
 			it('responds with status code 200');	
