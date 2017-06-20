41f1917458e9e509a201bd661fade4958d41a4bb
bgveenstra <b.g.veenstra@gmail.com>
2017-06-20 15:33:53 -0700

    add unused test helper

--- test/testHelpers.js
+++ test/testHelpers.js
@@ -1,13 +1,7 @@
-var err = new Error('TEST PENDING');
-function pending() {
-  throw err;
-}
-
 function context(ctxDescription) {
   return '(context ' + ctxDescription + ')';
 }
 
 module.exports = {
-  context: context,
-  pending: pending
+  context: context
 };
